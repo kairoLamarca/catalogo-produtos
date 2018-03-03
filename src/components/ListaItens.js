43bcd54bd4c1d79fdc097517380385f2e8c646bb
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-03-03 16:42:30 -0300

    utilização de state e map para mostrar os itens
    utilização de state e map para mostrar os itens

--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
-    View
+    View,
+    Text
 } from 'react-native';
 import axios from 'axios';
 import Itens from './Itens';
@@ -8,10 +9,12 @@ import Itens from './Itens';
 export default class ListaItens extends Component {
 
     //1º
-    // constructor(props) {
-    //     super(props);
-    //     console.log('Construindo a aplicação');
-    // }
+    constructor(props) {
+        super(props);
+        //console.log('Construindo a aplicação');
+
+        this.state = { listaItens: [] };
+    }
 
     //2º
     componentWillMount() {
@@ -20,9 +23,7 @@ export default class ListaItens extends Component {
         //requisição HTTP
         //retorna uma promise
         axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-            .then((response) => {
-                console.log(response);
-            })
+            .then(response => { this.setState({ listaItens: response.data }); })
             .catch(() => console.log('erro'));
     }
 
@@ -36,9 +37,7 @@ export default class ListaItens extends Component {
         //console.log('Objeto é renderizado');
         return (
             <View>
-                <Itens />
-                <Itens />
-                <Itens />
+                {this.state.listaItens.map(item => (<Text key={item.titulo}>{item.titulo}</Text>))}
             </View>
         );
     }
